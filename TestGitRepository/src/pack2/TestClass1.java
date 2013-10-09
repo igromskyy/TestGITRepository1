c94433dabdd0eb76fca4bf58225ee45b274a5a5f
IvanGromskyy <igromskyy@gmail.com>
2013-10-09 22:27:49 +0300

    Create TestGitRepository Project

---
+++ TestGitRepository/src/pack2/TestClass1.java
@@ -0,0 +1,10 @@
+package pack2;
+
+public class TestClass1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hellow from TestClass1");
+
+	}
+
+}
---
+++ TestGitRepository/src/pack2/TestClass2.java
@@ -0,0 +1,10 @@
+package pack2;
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hellow from TestClass2");
+
+	}
+
+}
